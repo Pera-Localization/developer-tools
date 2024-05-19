ed4d79283fdaa9eb9c8971b4243bf4e8830e3c10
James Roland <james.a.roland@gmail.com>
2024-05-19 00:10:07 -0700

    Changing config schema

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,17 +4,58 @@ import { z } from "zod";
 export const configSchema = z.object({
   source: z.object({
     locale: z.enum(supportedLocales),
-    path: z.string(),
+    path: z
+      .string()
+      .describe(
+        'Path to the source locale file. Example: "src/locales/en.json"'
+      ),
   }),
   targets: z.record(
     z.enum(supportedLocales),
     z.object({
-      path: z.string(),
+      path: z
+        .string()
+        .describe(
+          'Path to the target locale file. Example: "src/locales/fr.json". The file will be created if it does not exist when publishing changes.'
+        ),
     })
   ),
-  detectionBranch: z.string().optional().default("main"),
-  translationBranch: z.string().optional().default("main"),
-  publishBranch: z.string().optional().default("main"),
+  triggers: z.object({
+    detection: z.object({
+      mergeBranch: z
+        .string()
+        .optional()
+        .default("main")
+        .describe(
+          "When a pull request is merged into this branch, new translations will be detected and enqueued for translation."
+        ),
+    }),
+    translation: z.object({
+      mergeBranch: z
+        .string()
+        .optional()
+        .default("main")
+        .describe(
+          "When a pull request is merged into this branch, any enqueued translations will be automatically translated."
+        ),
+    }),
+    publish: z.object({
+      mergeBranch: z
+        .string()
+        .optional()
+        .default("main")
+        .describe(
+          "When a pull request is merged into this branch, a new pull request will be created to publish any new translations."
+        ),
+      baseBranch: z
+        .string()
+        .optional()
+        .default("main")
+        .describe(
+          "The branch to use as the base for the pull request that publishes new translations."
+        ),
+    }),
+  }),
 });
 
 // Input schemas
